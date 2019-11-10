fb29c61a29192ce852a78637452d0305f6f7d6fd
Tundeh <jamiu4freedom@gmail.com>
2019-11-10 07:56:07 +0100

    database dependencies

--- API/server.js
+++ API/server.js
@@ -1,1 +1,8 @@
-console.log("server running");
+const http = require("http");
+
+const app = require("./app");
+
+app.set("port", process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
